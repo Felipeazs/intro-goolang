c4a402e9acf33727e2b4eb2e511c4745dbaf03bd
felipeazs <felipeazs@gmail.com>
2022-09-30 23:15:10 -0300

    String interpolation

--- main.go
+++ main.go
@@ -10,6 +10,18 @@ func main(){
     fmt.Printf("Welcome to our %v booking application\n", conferenceName)
     fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
     fmt.Println("Get your tickets here to attend")
+
+
+    var userName string
+    var userTickets int
+    //ask user
+
+    userName = "Felipe"
+    userTickets = 2
+
+    fmt.Printf("userName is %T, userTickets are %T", userName, userTickets)
+    fmt.Printfn("User %v booked %v tickets.", userName, userTickets)
+
 }
 
 
