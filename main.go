5e6e4d9a70318d1bbf1bf6422a654bf248cd94e7
felipeazs <felipeazs@gmail.com>
2022-09-30 23:38:46 -0300

    variable declaration and Scan inputs

--- main.go
+++ main.go
@@ -3,24 +3,37 @@ package main
 import "fmt"
 
 func main(){
-    var conferenceName = "Go conference"
+    conferenceName := "Go conference"
     const conferenceTickets = 50
-    var remainingTickets = 50
+    remainingTickets := 50
     
     fmt.Printf("Welcome to our %v booking application\n", conferenceName)
     fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
     fmt.Println("Get your tickets here to attend")
 
 
-    var userName string
+    var firstName string
+    var lastName string
+    var email string
     var userTickets int
+
+    fmt.Printf("firstName is %T\n", firstName)
+    
+    fmt.Println("Enter your first name: ")
+    fmt.Scan(&firstName) //print the value in memory -- & is a pointer
+
+    fmt.Println("Enter your last name: ")
+    fmt.Scan(&lastName) //print the value in memory -- & is a pointer
+
+    fmt.Println("Enter your email address: ")
+    fmt.Scan(&email)
+    
+    fmt.Println("Enter number of tickets: ")
+    fmt.Scan(&userTickets)
     //ask user
 
-    userName = "Felipe"
-    userTickets = 2
 
-    fmt.Printf("userName is %T, userTickets are %T\n", userName, userTickets)
-    fmt.Printf("User %v booked %v tickets.\n", userName, userTickets)
+    fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 
 }
 
