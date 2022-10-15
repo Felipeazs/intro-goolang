efad6e5465181e12b407ef0032138b00ca3017f3
felipeazs <felipeazs@gmail.com>
2022-10-15 20:09:43 -0300

    last update

--- main.go
+++ main.go
@@ -2,55 +2,43 @@ package main
 
 import "fmt"
 
-func main(){
-    conferenceName := "Go conference"
-    const conferenceTickets = 50
-    var remainingTickets uint = 50
-    
-    fmt.Printf("Welcome to our %v booking application\n", conferenceName)
-    fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
-    fmt.Println("Get your tickets here to attend")
+func main() {
+	conferenceName := "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets uint = 50
 
-    var bookings []string 
+	fmt.Printf("Welcome to our %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
 
-    var firstName string
-    var lastName string
-    var email string
-    var userTickets uint
+	bookings := []string{}
 
-    fmt.Printf("firstName is %T\n", firstName)
-    fmt.Printf("userTickets is %T\n", userTickets)
-    
-    fmt.Println("Enter your first name: ")
-    fmt.Scan(&firstName) //print the value in memory -- & is a pointer
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 
-    fmt.Println("Enter your last name: ")
-    fmt.Scan(&lastName) //print the value in memory -- & is a pointer
+	fmt.Printf("firstName is %T\n", firstName)
+	fmt.Printf("userTickets is %T\n", userTickets)
 
-    fmt.Println("Enter your email address: ")
-    fmt.Scan(&email)
-    
-    fmt.Println("Enter number of tickets: ")
-    fmt.Scan(&userTickets)
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName) //print the value in memory -- & is a pointer
 
-    bookings = append(bookings, firstName + " " + lastName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName) //print the value in memory -- & is a pointer
 
-    fmt.Printf("The whole array: %v\n", bookings)
-    fmt.Printf("The first value: %v\n", bookings[0])
-    fmt.Printf("Array type: %T\n", bookings)
-    fmt.Printf("Array length: %v\n", len(bookings))
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
 
-    remainingTickets = remainingTickets - userTickets
-    
-    fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-    fmt.Printf("%v tickets remaining", remainingTickets)
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
 
+	bookings = append(bookings, firstName+" "+lastName)
 
+	remainingTickets = remainingTickets - userTickets
 
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining", remainingTickets)
+
+	fmt.Printf("These are all our bookings: %v\n", bookings)
 }
-
-
-
-
-
-
